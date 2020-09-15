0558feb84827ef28fde72f86dcb523798a3841de
Nahuel Krowicki <nkrowicki@nubedi.com>
2020-09-15 19:39:51 -0300

    Start with reducer

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,29 +1,55 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from '../../hooks/useForm'
+import { useDispatch } from 'react-redux'
+import { startLoginEmailPassword } from '../../action/auth'
 
 const LoginScreen = () => {
+
+    const dispatch = useDispatch()
+
+    const [formValues, handleInputChange] = useForm({
+        email: 'nando@gmail.com',
+        password: '123456'
+    });
+
+    const { email, password }  = formValues;
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        dispatch(startLoginEmailPassword(email, password));
+
+    }
+
     return (
         <div>
                 <h3 className="auth__title">Login</h3>
                 
-                <form>
+                <form
+                    onSubmit={handleLogin}
+                >
                     <input
                         type="text"
                         placeholder="Email"
                         name="email"
                         className="auth__input"
+                        value={email}
+                        onChange={handleInputChange}
                         />
 
                     <input
                         type="password"
                         placeholder="Password"
                         name="password"
-                        className="auth__input"
+                        className="auth__input" 
+                        value={password}
+                        onChange={handleInputChange}
                         />
                     
                     <button 
                         type="submit"
-                        className="btn btn-primary btn-block"
+                        className="btn btn-primary btn-block" 
                         >Login</button>
 
                     <hr />
